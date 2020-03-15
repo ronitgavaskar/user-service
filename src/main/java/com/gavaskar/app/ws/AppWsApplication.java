07406ab694e9b40e95f154dc2e9e97f74202a6d1
ronitgavaskar <ronitgavaskar@gmail.com>
2020-03-14 21:37:20 -0700

    Deploying war instead of jar

--- src/main/java/com/gavaskar/app/ws/AppWsApplication.java
+++ src/main/java/com/gavaskar/app/ws/AppWsApplication.java
@@ -3,11 +3,13 @@ package com.gavaskar.app.ws;
 import com.gavaskar.app.ws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class AppWsApplication {
+public class AppWsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AppWsApplication.class);
@@ -20,5 +22,10 @@ public class AppWsApplication {
     @Bean
     public SpringApplicationContext springApplicationContext() {
         return new SpringApplicationContext();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return createSpringApplicationBuilder().sources(AppWsApplication.class);
     }
 }
